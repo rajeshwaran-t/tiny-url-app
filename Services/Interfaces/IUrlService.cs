331d4c7ad3e7155844238a2ffeabe079a7230782
Rajeshwaran T <rajeshwaranrpt@gmail.com>
2026-03-15 23:39:27 +0530

    Initial commit - TinyURL project

---
+++ Services/Interfaces/IUrlService.cs
@@ -0,0 +1,12 @@
+﻿using TinyUrl.Models;
+
+namespace TinyUrl.Services.Interfaces
+{
+    public interface IUrlService
+    {
+        Task<Url> CreateShortUrl(string originalUrl, bool isPrivate);
+        Task<List<Url>> GetAllUrls();
+        Task<string> RedirectUrl(string code);
+        Task DeleteUrl(int id);
+    }
+}
